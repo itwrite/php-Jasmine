067e32278a3b37dc763a3fe52257d70e6600c6a7
itwrite <990881086@qq.com>
2020-03-02 04:23:40 +0800

    新增一些特性

--- src/util/Str.php
+++ src/util/Str.php
@@ -284,4 +284,17 @@ class Str
 
         return str_replace(' ', '', $value);
     }
+
+    /**
+     * Returns the portion of string specified by the start and length parameters.
+     *
+     * @param  string  $string
+     * @param  int  $start
+     * @param  int|null  $length
+     * @return string
+     */
+    public static function substr($string, $start, $length = null)
+    {
+        return mb_substr($string, $start, $length, 'UTF-8');
+    }
 }
