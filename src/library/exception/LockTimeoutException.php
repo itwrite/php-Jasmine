6db63cfff60048f06e9e98951c6e6aa33973f1f9
itwrite <990881086@qq.com>
2020-03-02 04:20:51 +0800

    新增一些特性

---
+++ src/library/exception/LockTimeoutException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itwri
+ * Date: 2020/2/29
+ * Time: 10:23
+ */
+
+namespace Jasmine\library\exception;
+
+
+class LockTimeoutException extends \Exception
+{
+
+}
